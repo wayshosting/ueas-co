fabf96f1fe1c238c0517625f17b72133b8a3b225
M <hosting@waysall.com>
2016-06-20 04:06:23 +0500

    Certificate Verification moved to Google Sheets v1

--- accreditation/certificate-verification/search.js
+++ accreditation/certificate-verification/search.js
@@ -1,32 +1,65 @@
 /* JS File */
 
 // Start Ready
-$(document).ready(function() {  
+$(document).ready(function() {
+	$('input#search').focus();
 
 	// Icon Click Focus
 	$('div.icon').click(function(){
 		$('input#search').focus();
 	});
-	
+
 	// Live Search
 	// On Search Submit and Get Results
 	function search() {
 		var query_value = $('input#search').val();
 		$('b#search-string').html(query_value);
 		if(query_value !== ''){
-			$.ajax({
-				type: "POST",
-				url: "http://ueas.co.uk/accreditation/certificate-verification/search.php",
-				data: { query: query_value },
-				cache: false,
-				success: function(html){
-					$("ul#results").html(html);
-				}
-			});
-		}return false;    
+			var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1WQIHuf0SgnTU1QRG-qMxAJccmsGaPJ_FF7nVSdnT-m8/edit#gid=516585884';
+
+			var myQuery = "select B,C,D,E,H,I,M where B contains '" + query_value + "' order by A asc";
+
+			//error handler
+			var getError = function (error, options, response) {
+				 //console.log(error, options, response);
+
+				 //no result found message
+				 $("h2#search-error").text("");
+				 if (response.html == "") {
+					 $("h2#search-error").text("No matching certificate found. Plese check your certificate number. It is case sensitive. e.g. type A2 not a2.")
+				 }
+				 changeColor();
+			 };
+
+		  // Compile the Handlebars template for HR leaders.
+		  var HRTemplate = Handlebars.compile($('#hr-template').html());
+
+		  $('#results').sheetrock({
+		   url: mySpreadsheet,
+		   query: myQuery,
+		   fetchSize: 30,
+		   rowTemplate: HRTemplate,
+			 callback: getError
+		  });
+
+		}	return false;
 	}
 
-	$("input#searchbutton").live("click", function(e) {
+	//set certificate status colors
+	function changeColor(){
+		$("h2.status-display").each(function(){
+			//get certificate status
+			var status_value = $(this).text();
+			//apply colors
+			if (status_value == "Active") {
+				$(this).css("color", "green")
+			}else{
+				$(this).css("color", "orange")
+			};
+		});
+	}
+
+	$("input#searchbutton").on("click", function(e) {
 		// Set Timeout
 		clearTimeout($.data(this, 'timer'));
 
@@ -40,8 +73,14 @@ $(document).ready(function() {
 		}else{
 			$("ul#results").fadeIn();
 			$('h4#results-text').fadeIn();
-			$(this).data('timer', setTimeout(search, 100));
-		};
+			$(this).data('timer', setTimeout(search, 200));
+			};
+	});
+
+
+	//reload page to conduct new search
+	$("input#resetbutton").click(function(){
+		location.reload();
 	});
 
 });
